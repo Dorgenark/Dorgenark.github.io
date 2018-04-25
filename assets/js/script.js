0dfc7cd25d530fbbb5c9588921fea33a70a6bdc7
Dorgenark <simonromainville@hotmail.com>
2018-04-25 13:39:02 +0200

    favicon + animations jQuery/CSS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+// ABOUT & CONTACT
+// Show div
 function openPopup(el) {
 	if ($('#' + el).is(':visible')) {
 		$('#' + el).slideUp(200);
@@ -6,7 +8,27 @@ function openPopup(el) {
 	   $('#' + el).slideToggle(300);   
 	}
 }
-
+// Hide div
 function closePopup() {
     $('.popup').slideUp(200);
 }
+
+
+// BACK TO TOP
+// Scroll detection
+window.addEventListener("scroll", function() { 
+   if(window.scrollY > 380) {
+      $('.btn-top').fadeIn(300);
+   }
+   else {
+      $('.btn-top').fadeOut(200);
+   }
+},false);
+// Animation to top
+$('.btn-top a').on("click", function(){
+	var scroll = $(this).attr("href");
+    $('html, body').animate({
+        scrollTop:$(scroll).offset().top
+	}, 600);
+	return false;
+});
